f8c2c9e95b6e731e679ee78cac366810d0eb6104
GCostill <gaby123le@gmail.com>
2021-09-14 18:53:17 -0400

    CheckoutDataCompleted

--- LibraryCheckOut.Data/Checkout.cs
+++ LibraryCheckOut.Data/Checkout.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace LibraryCheckOut.Data
     public class Checkout
     {
         [Key]
+        [Required]
         public int Checkout_Id { get; set; }
+        public DateTime CheckoutDate { get; set; }
+        public DateTime CheckoutDueDate
+        {
+            get
+            {
+                DateTime dueDate = CheckoutDate.AddDays(7);
+                return dueDate;
+            }
+        }
+        [ForeignKey(nameof(Member))]
+        [Required]
+        public int Member_Id { get; set; }
+
+        [ForeignKey(nameof(Media))]
+        [Required]
+        public List<Media> ListOfItems { get; set; }
+        public int TotalNumberOfItems { get; set; }
+
     }
 }
