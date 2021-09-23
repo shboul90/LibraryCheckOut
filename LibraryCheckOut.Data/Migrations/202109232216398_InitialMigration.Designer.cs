c02749a00f3f1bd82168ea733892292e8ec87d1b
GCostill <gaby123le@gmail.com>
2021-09-23 19:12:39 -0400

    Commit

--- LibraryCheckOut.Data/Migrations/202109232216398_InitialMigration.Designer.cs
+++ LibraryCheckOut.Data/Migrations/202109232216398_InitialMigration.Designer.cs
@@ -7,13 +7,13 @@ namespace LibraryCheckOut.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class ICollectionsAdded : IMigrationMetadata
+    public sealed partial class InitialMigration : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(ICollectionsAdded));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialMigration));
         
         string IMigrationMetadata.Id
         {
-            get { return "202109212255211_ICollectionsAdded"; }
+            get { return "202109232216398_InitialMigration"; }
         }
         
         string IMigrationMetadata.Source
